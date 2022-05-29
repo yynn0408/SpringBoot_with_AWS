b89f38b319abc9335f84879746a16700f562a47c
yujin <yynn0408@gmail.com>
2022-05-29 15:07:26 +0900

    게시물 수정

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -19,7 +21,13 @@ public class IndexController {
 
     @GetMapping("/posts/save")
     public String postsSave(){
-        return "posts-save";//mustache 플러그 인을 사용하여 자동으로 /resources/temmplates경로에서 posts-save.mustache를 호출함
+        return "posts-save";//mustache 플러그 인을 사용하여 자동으로 /resources/templates경로에서 posts-save.mustache를 호출함
+    }
+    @GetMapping("posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto =postsService.findById(id);
+        model.addAttribute("posts",dto);
+        return "posts-update";
     }
 
 
