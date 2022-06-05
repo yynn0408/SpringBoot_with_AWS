a02af2adb0fcb56897fac2be14fae63060568ea1
yujin <yynn0408@gmail.com>
2022-06-06 00:23:01 +0900

    스프링 시큐리티 적용 후 테스트코드 에러 수정 - 테스트 코드에서는 user로그인을 못하기 때문에 가짜 계정으로 확인 - mockmvc 생성 -> 가짜 객체 생성하여 mvc 동작 테스트

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화 (insert, modified date 관리)
+//@EnableJpaAuditing //JPA Auditing 활성화 (insert, modified date 관리)
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
